0cab5f7fe4dab95bd2119cf2eeb527a5b023781c
Abdur Rahim <abdurrahimcmt@gmail.com>
2021-06-16 23:29:27 +0600

    firstCommit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperShop.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<ApplicationType> ApplicationType { get; set; }
+    }
+}
